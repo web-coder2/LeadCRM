925f13d89a032fd3e1419dff8a170586f4d6fad0
web-coder <yasher18@bk.ru>
2025-03-19 14:34:06 +0300

    улучшен дизайн добавлены 2 дрона (вторйо доделать) и стурктура изменена лидов

---
+++ croner.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+const crone = require('node-cron')
+const { json } = require('body-parser')
+
+crone.schedule('0 * * * *', () => {
+    console.log('change-user-status crone has been started')
+
+    let userFilePath = path.join(__dirname, 'users.json')
+    console.log(userFilePath)
+    let usersData = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'))
+    console.log(usersData)
+
+    for (let i = 0; i < usersData.length; i++) {
+        usersData[i].status = false
+    }
+    
+    let unparsedUsersData = JSON.stringify(usersData, null, 2)
+    fs.writeFileSync(userFilePath, unparsedUsersData)
+    console.log('rewrited users-statuses')
+});
+
+crone.schedule('0 * * * *', () => {
+    console.log('lead-maxination crone has been started')
+
+    let leadsPath = path.join(__dirname, 'data.json')
+    let generalLeadsPath = path.join(__dirname, 'generalLeads.json')
+
+    let brokerLeads = JSON.parse(fs.readFileSync(leadsPath, 'utf-8'))
+    let generalLeadsData = JSON.parse(fs.readFileSync(generalLeadsPath, 'utf-8'))
+
+    for (let i = 0; i < brokerLeads.length; i++) {
+        // если каждый час крон замечает что у лида не обработан
+        if (brokerLeads[i].isSend == false) {
+            brokerLeads[i].broker = ''  // убирает у лида какому брокеру
+            generalLeadsData.push(brokerLeads[i]) // добавляет в общаг
+        }
+    }
+})
